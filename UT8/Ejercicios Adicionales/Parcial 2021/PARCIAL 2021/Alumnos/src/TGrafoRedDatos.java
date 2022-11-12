d049ff177d4805e7a43a149ec59e36895f2fa2e8
MARTÍN CARABALLO <MARTIN.CARABALLO@correo.ucu.edu.uy>
2022-11-12 10:52:49 -0300

    .

---
+++ UT8/Ejercicios Adicionales/Parcial 2021/PARCIAL 2021/Alumnos/src/TGrafoRedDatos.java
@@ -0,0 +1,27 @@
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author ernesto
+ */
+public class TGrafoRedDatos extends TGrafoNoDirigido implements IGrafoRedDatos {
+
+    public TGrafoRedDatos(Collection<TVertice> vertices, Collection<TArista> aristas) {
+        super(vertices, aristas);
+    }
+
+    @Override
+    public LinkedList<TVertice> rutaMenosSaltos(Comparable origen, Comparable destino) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
+  
+}
