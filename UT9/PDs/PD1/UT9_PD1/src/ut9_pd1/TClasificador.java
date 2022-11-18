24b0515e6d13eeee9e97591ed77e59f0bdb3e02f
MARTÍN CARABALLO <MARTIN.CARABALLO@correo.ucu.edu.uy>
2022-11-18 14:50:58 -0300

    UT9_PD1 start

---
+++ UT9/PDs/PD1/UT9_PD1/src/ut9_pd1/TClasificador.java
@@ -0,0 +1,244 @@
+
+public class TClasificador {
+
+    public static final int METODO_CLASIFICACION_INSERCION = 1;
+    public static final int METODO_CLASIFICACION_SHELL = 2;
+    public static final int METODO_CLASIFICACION_BURBUJA = 3;
+    public static final int METODO_CLASIFICACION_QUICKSORT = 4;
+
+    /**
+     * Punto de entrada al clasificador
+     *
+     * @param metodoClasificacion
+     * @param orden
+     * @param tamanioVector
+     * @return Un vector del tam. solicitado, ordenado por el algoritmo
+     * solicitado
+     */
+    public int[] clasificar(int[] datosParaClasificar, int metodoClasificacion) {
+        switch (metodoClasificacion) {
+            case METODO_CLASIFICACION_INSERCION:
+                return ordenarPorInsercion(datosParaClasificar);
+            case METODO_CLASIFICACION_SHELL:
+                return ordenarPorShell(datosParaClasificar);
+            case METODO_CLASIFICACION_BURBUJA:
+                return ordenarPorBurbuja(datosParaClasificar);
+            case METODO_CLASIFICACION_QUICKSORT:
+                return ordenarPorQuickSort(datosParaClasificar);
+
+            default:
+                System.err.println("Este codigo no deberia haberse ejecutado");
+                break;
+        }
+        return datosParaClasificar;
+    }
+
+    private void intercambiar(int[] vector, int pos1, int pos2) {
+        int temp = vector[pos2];
+        vector[pos2] = vector[pos1];
+        vector[pos1] = temp;
+    }
+
+    /**
+     * @param datosParaClasificar
+     * @return
+     */
+    private int[] ordenarPorShell(int[] datosParaClasificar) {
+        int j, inc;
+        int[] incrementos = new int[]{3223, 358, 51, 10, 3, 1};
+
+        for (int posIncrementoActual = 0; posIncrementoActual < incrementos.length; posIncrementoActual++) {
+            inc = incrementos[posIncrementoActual];
+            if (inc < (datosParaClasificar.length / 2)) {
+                for (int i = inc; i < datosParaClasificar.length; i++) {
+                    j = i - inc;
+                    while (j >= 0) {
+                        if (datosParaClasificar[j] > datosParaClasificar[j + inc]) {
+                            intercambiar(datosParaClasificar, j, j + inc);
+                        }
+                        j = j - inc;
+                    }
+                }
+            }
+        }
+        return datosParaClasificar;
+    }
+
+    /**
+     * @param datosParaClasificar
+     * @return
+     */
+    protected int[] ordenarPorInsercion(int[] datosParaClasificar) {
+        if (datosParaClasificar != null) {
+            for (int i = 1; i < datosParaClasificar.length; i++) {
+                int j = i - 1;
+                while ((j >= 0) && (datosParaClasificar[j + 1] < datosParaClasificar[j])) {
+                    intercambiar(datosParaClasificar, j + 1, j);
+                    j--;
+                }
+            }
+            return datosParaClasificar;
+        }
+        return null;
+    }
+
+    private int[] ordenarPorBurbuja(int[] datosParaClasificar) {
+        int n = datosParaClasificar.length - 1;
+        for (int i = 0; i <= n; i++) {
+            for (int j = n; j >= (i + 1); j--) {
+                if (datosParaClasificar[j] < datosParaClasificar[j - 1]) {
+                    intercambiar(datosParaClasificar, j - 1, j);
+                }
+            }
+        }
+        return datosParaClasificar;
+    }
+
+    public boolean estaOrdenado(int[] vector) {
+        for (int i = 0; i < vector.length - 1; i++) {
+            if (vector[i] >= vector[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected int[] ordenarPorQuickSort(int[] datosParaClasificar) {
+        quicksort(datosParaClasificar, 0, datosParaClasificar.length - 1);
+        return datosParaClasificar;
+    }
+    
+    /*
+    protected int[] ordenarPorQuickSort(int[] datosParaClasificar) {
+        int altura = quicksort(datosParaClasificar, 0, datosParaClasificar.length - 1);
+        System.out.println("Altura: " + altura);
+        return datosParaClasificar;
+    }
+    */
+
+    private void quicksort(int[] entrada, int i, int j) {
+        int izquierda = i;
+        int derecha = j;
+
+        int posicionPivote = encuentraPivote(izquierda, derecha, entrada);
+        if (posicionPivote >= 0) {
+            int pivote = entrada[posicionPivote];
+            while (izquierda <= derecha) {
+                while ((entrada[izquierda] < pivote) && (izquierda < j)) {
+                    // L se mueve a la derecha
+                    izquierda++;
+                }
+
+                while ((pivote < entrada[derecha]) && (derecha > i)) {
+                    // R se mueve a la izquierda
+                    derecha--;
+                }
+
+                if (izquierda <= derecha) {
+                    intercambiar(entrada, derecha, izquierda);
+                    izquierda++;
+                    derecha--;
+                }
+            }
+
+            if (i < derecha) {
+                quicksort(entrada, i, derecha);
+            }
+            if (izquierda < j) {
+                quicksort(entrada, izquierda, j);
+            }
+        }
+    }
+    
+    /*
+    private int quicksort(int[] entrada, int i, int j) {
+        int izquierda = i;
+        int derecha = j;
+
+        int posicionPivote = encuentraPivoteRandom(izquierda, derecha, entrada, 42);
+        if (posicionPivote >= 0) {
+            // El pivote es el elemento de la entrada cuyo índice es "posicionPivote".
+            int pivote = entrada[posicionPivote];
+            while (izquierda <= derecha) {
+                while ((entrada[izquierda] < pivote) && (izquierda < j)) {
+                    // L debe moverse a la derecha.
+                    izquierda++;
+                }
+
+                while ((pivote < entrada[derecha]) && (derecha > i)) {
+                    // R debe moverse a la izquierda.
+                    derecha--;
+                }
+
+                if (izquierda <= derecha) {
+                    intercambiar(entrada, derecha, izquierda);
+                    izquierda++;
+                    derecha--;
+                }
+            }
+
+            int maxAltura = 0;
+
+            // Un tercer error relacionado con las llamadas recursivas,
+            // respecto a los límites de los subarreglos.
+            if (i < derecha) {
+                // derecha es el fin del subvector izquierdo.
+                int alturaIzquierda = quicksort(entrada, i, derecha);
+                if (maxAltura < alturaIzquierda) {
+                    maxAltura = alturaIzquierda;
+                }
+            }
+            if (izquierda < j) {
+                // izquierda es el fin del subvector derecho.
+                int alturaDerecha = quicksort(entrada, izquierda, j);
+                if (maxAltura < alturaDerecha) {
+                    maxAltura = alturaDerecha;
+                }
+            }
+
+            return maxAltura + 1;
+        }
+        return 0;
+    }
+    */
+
+    private int encuentraPivote(int izquierda, int derecha, int[] entrada) {
+        int medio = (izquierda + derecha) / 2;
+        if ((izquierda < derecha && derecha < medio) || (medio < izquierda && izquierda > derecha)) {
+            return derecha;
+        } else if ((derecha < izquierda && izquierda < medio) || (medio < izquierda && izquierda < derecha)) {
+            return izquierda;
+        } else {
+            return medio;
+        }
+    }
+
+    public static void main(String args[]) {
+        TClasificador clasif = new TClasificador();
+        GeneradorDatosGenericos gdg = new GeneradorDatosGenericos();
+        int[] vectorAleatorio = gdg.generarDatosAleatorios();
+        int[] vectorAscendente = gdg.generarDatosAscendentes();
+        int[] vectorDescendente = gdg.generarDatosDescendentes();
+
+        /*int[] resAleatorio = clasif.clasificar(vectorAleatorio,
+                METODO_CLASIFICACION_INSERCION);
+        for (int i = 0; i < resAleatorio.length; i++) {
+            System.out.print(resAleatorio[i] + " ");
+        }
+         */
+        int[] resQui = clasif.clasificar(vectorAleatorio, METODO_CLASIFICACION_QUICKSORT);
+        for (int i = 0; i < resQui.length; i++) {
+            System.out.print(resQui[i] + " ");
+        }
+        /*int[] resAscendente = clasif.clasificar(vectorAscendente,
+				METODO_CLASIFICACION_INSERCION);
+		for (int i = 0; i < resAscendente.length; i++) {
+			System.out.print(resAscendente[i] + " ");
+		}
+		int[] resDescendente = clasif.clasificar(vectorDescendente,
+				METODO_CLASIFICACION_INSERCION);
+		for (int i = 0; i < resDescendente.length; i++) {
+			System.out.print(resDescendente[i] + " ");
+		}*/
+    }
+}
